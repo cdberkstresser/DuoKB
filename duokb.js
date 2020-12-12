f68b1c7cbb191892bf63b81b366a230540f906b7
Caleb Berkstresser <cdberkstresser@protonmail.com>
2020-12-12 12:06:07 -0600

    Making language dynamic

--- duokb.js
+++ duokb.js
@@ -2,35 +2,50 @@
  * This will add the ability to change keyboard input to the target type of duolingo's input boxes.
  */
 
+/** The friendly name of the language that DuoLingo uses in the "Type in" placeholders. */
+var language = "";
+/** The language code of the language being used.  Ideally we would only use this, but "lang ="" isn't always set. */
+var languageCode = "";
+/** Duolingo seems to be manipulating the same box rather than reloading, so watch it for changes. */
 window.onload = function() {
     setInterval(load, 200);
+    this
 }
 
+/** Set the input correctly as the input boxes change. */
 function load() {
-    var inputBoxes = document.querySelectorAll("textarea");
-    inputBoxes.forEach(e => {
-        if (e.getAttribute('lang') == 'he') {
+    // see if any text boxes or text areas have appeared that we want to translate
+    document.querySelectorAll("textarea, input[type='text']").forEach(e => {
+        language = e.getAttribute('placeholder') && e.getAttribute('placeholder').startsWith('Type in ') ? e.getAttribute('placeholder').replace('Type in ', '') : '';
+        //if so and we support the language, set the language code
+        if (supportedTranslations[language]) {
+            languageCode = supportedTranslations[language];
             e.addEventListener("keydown", processKey);
-            e.addEventListener("change", addSpace);
         }
     });
-
 }
 
-function addSpace() {
-    this.value = this.value + ' ';
-    console.log("space added");
-}
+/** Set up an event to prevent the last word typed from being overwritten on submit and show DuoLingo that we have typed something in the box since we are suppressing normal behavior of input keys. */
+var showBoxHasBeenTypedInEvent = new Event('input', {
+    bubbles: true,
+    cancelable: true,
+});
 
+/** Replace keys with the correct layout if we are set to actively utilize that layout. */
 function processKey(e) {
-    if (e.code == 'Enter') {
-        this.blur();
-        return;
-    }
-    if (he && e.code in he) {
+    // if that translation table exists and if that key is set to be replaced in that translation table, change it.
+    if (window[languageCode] && e.code in window[languageCode]) {
+        // stop this key from doing what it usually does
         e.preventDefault();
+        // see where the cursor is at so we can preserve cursor position
         var startIndex = this.selectionStart;
         var endIndex = this.selectionEnd;
-        this.value = this.value.substring(0, startIndex) + he[e.code] + this.value.substring(endIndex);
+        // do the replacement in the box
+        this.value = this.value.substring(0, startIndex) + window[languageCode][e.code] + this.value.substring(endIndex);
+        // let DuoLingo know we have typed in the box 
+        this.dispatchEvent(showBoxHasBeenTypedInEvent);
+        // move the cursor forward
+        this.selectionStart = startIndex + 1;
+        this.selectionEnd = endIndex + 1;
     }
 }
--- translations.js
+++ translations.js
@@ -1,35 +1,84 @@
+/**
+ * This provides the functionality for capturing friendly names of languages as well as attaching them to language codes.
+ * 
+ * Note: Ideally, all we would use is the lang code indicated in the input box, but currently, DuoLingo may only utilize
+ * a placeholder "Type in xxxxxx" to indicate the language.  Would like to not need to depend on a placeholder text.
+ */
+
+
+/*
+ ***********SUPPORTED TRANSLATIONS WITH FRIENDLY NAMES***********
+ */
+var supportedTranslations = {
+    Greek: 'el',
+    Hebrew: 'he',
+}
+
+
 /*
  ***********KEYBOARD TRANSLATION OBJECTS***********
  */
+
+var el = {
+    KeyQ: ';',
+    KeyW: 'ς',
+    KeyE: 'ε',
+    KeyR: 'ρ',
+    KeyT: 'τ',
+    KeyY: 'υ',
+    KeyU: 'θ',
+    KeyI: 'ι',
+    KeyO: 'ο',
+    KeyP: 'π',
+    KeyA: 'α',
+    KeyS: 'σ',
+    KeyD: 'δ',
+    KeyF: 'φ',
+    KeyG: 'γ',
+    KeyH: 'η',
+    KeyJ: 'ξ',
+    KeyK: 'κ',
+    KeyL: 'λ',
+    KeyZ: 'ζ',
+    KeyX: 'χ',
+    KeyC: 'ψ',
+    KeyV: 'ω',
+    KeyB: 'β',
+    KeyN: 'ν',
+    KeyM: 'μ',
+};
+
 var he = {
-    KeyA: 'ש',
-    KeyB: 'נ',
-    KeyC: 'ב',
-    KeyD: 'ג',
+    Backquote: ';',
+    KeyQ: '/',
+    KeyW: '\'',
     KeyE: 'ק',
+    KeyR: 'ר',
+    KeyT: 'א',
+    KeyY: 'ט',
+    KeyU: 'ו',
+    KeyI: 'ן',
+    KeyO: 'ם',
+    KeyP: 'פ',
+    KeyA: 'ש',
+    KeyS: 'ד',
+    KeyD: 'ג',
     KeyF: 'כ',
     KeyG: 'ע',
     KeyH: 'י',
-    KeyI: 'ן',
     KeyJ: 'ח',
     KeyK: 'ל',
     KeyL: 'ך',
-    KeyM: 'צ',
-    KeyN: 'מ',
-    KeyO: 'ם',
-    KeyP: 'פ',
-    KeyR: 'ר',
-    KeyS: 'ד',
-    KeyT: 'א',
-    KeyU: 'ו',
-    KeyV: 'ה',
-    KeyX: 'ס',
-    KeyY: 'ט',
-    KeyZ: 'ז',
-    Quote: ',',
     Semicolon: 'ף',
+    Quote: ',',
+    KeyZ: 'ז',
+    KeyX: 'ס',
+    KeyC: 'ב',
+    KeyV: 'ה',
+    KeyB: 'נ',
+    KeyN: 'מ',
+    KeyM: 'צ',
     Comma: 'ת',
     Period: 'ץ',
     Slash: '.',
-    BackQuote: ';',
 };
